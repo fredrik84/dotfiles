4549c86fe699c270da894ade94af0227c7db8043
Fredrik Eriksson <feriksson@baffinbaynetworks.com>
2025-04-04 14:30:21 +0200

    Add support for identifing MacOS

--- scripts/01-formulaes.sh
+++ scripts/01-formulaes.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-FILE="schemas/$(echo $0 | cut -c '4-' | sed -e 's#.sh\$#.txt#')"
-brew install $(cat $FILE | xargs)
+if [[ "$(uname)" == "Darwin" ]]; then
+  FILE="schemas/$(echo $0 | cut -c '4-' | sed -e 's#.sh\$#.txt#')"
+  brew install $(cat $FILE | xargs)
+fi
